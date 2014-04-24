7d3882973bf76f2229d6b3e0ded82362d867c1ae
Fernando Redondo <fredondo@pronoide.com>
2014-04-25 01:24:37 +0200

    controller skelleton and its unit test

---
+++ test/unit/sesamestreet/MuppetDiscoveryControllerSpec.groovy
@@ -0,0 +1,20 @@
+package sesamestreet
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(MuppetDiscoveryController)
+class MuppetDiscoveryControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
