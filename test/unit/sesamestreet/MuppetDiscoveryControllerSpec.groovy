7f2a97d969780637da2101926190f675795b2a36
Fernando Redondo <fredondo@pronoide.com>
2014-04-25 01:51:11 +0200

    controller unit test cases

--- test/unit/sesamestreet/MuppetDiscoveryControllerSpec.groovy
+++ test/unit/sesamestreet/MuppetDiscoveryControllerSpec.groovy
@@ -1,20 +1,43 @@
 package sesamestreet
 
-import grails.test.mixin.TestFor
+import grails.test.mixin.*
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(MuppetDiscoveryController)
+@Mock([MuppetDiscoveryService, Muppet])
 class MuppetDiscoveryControllerSpec extends Specification {
 
     def setup() {
+		new Muppet(name: 'Ernie', color: "orange", hasFur: false, gender: Muppet.MALE).save()
     }
 
-    def cleanup() {
+    def "test Found Perfect Muppet"() {
+		given:
+			request.method = 'POST'
+			def params = [color: 'orange', hasFur: false ]
+			controller.params.putAll(params)
+		when:
+			controller.search()
+		then:		
+			view == '/muppetDiscovery/found'  
+			flash.msg.startsWith('Congrats')
+			model.muppet.name == 'Ernie'
     }
-
-    void "test something"() {
-    }
+	
+	def "test Not Found Perfect Muppet"() {
+		given:
+			request.method = 'POST'
+			def params = [color: 'yellow', hasFur: false ]
+			controller.params.putAll(params)
+		when:
+			controller.search()
+		then:
+			view == '/muppetDiscovery/notfound'
+			flash.msg.startsWith('Sorry')
+			model.muppets.size() == 1
+	}
 }
+
